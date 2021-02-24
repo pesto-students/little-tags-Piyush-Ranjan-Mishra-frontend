e88de78817c858e192f856a652e9e920c3ae1abe
Piyush Ranjan Mishra <kintrycom@gmail.com>
2021-02-25 02:11:03 +0530

    Library setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "./screens/homepage";
 
 function App() {
   return (
-    <div className="App">
-  Hello world
-    </div>
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          {/* <Route path="/about" component={About} />
+          <Route component={NoMatch} /> */}
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
