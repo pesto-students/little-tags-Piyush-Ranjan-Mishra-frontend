db0b40e58bdb2e4e9939bb0e6979258d1a4520c4
nitingpt000 <nitingpt000@gmail.com>
2021-03-01 17:07:16 +0530

    fix issue related to homepage

--- frontend/src/pages/homepage/homepage.screen.js
+++ frontend/src/pages/homepage/homepage.screen.js
@@ -10,7 +10,7 @@ const HomePageScreen = () => {
       <Helmet title="You Are Doing Great" />
       <HeroComponent />
       <CategoriesGridView />
-      <SecondaryCarousel />
+      {/* <SecondaryCarousel /> */}
     </>
   );
 };
